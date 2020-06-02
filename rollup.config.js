10e5dbb6912227f1090c28ae161fb0cdae8bd63e
kangchangyi <1004845773@qq.com>
2020-06-02 15:30:22 +0800

    fix(rollup): config 的 input 入口修改

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from "rollup-plugin-babel";
 import typescript from 'rollup-plugin-typescript2';
 
 export default {
-  input: "src/modules/index.ts",
+  input: "src/index.ts",
   output: {
     file: "./dist/index.js",
     format: "cjs",
