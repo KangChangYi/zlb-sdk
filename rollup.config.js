e96fd30af3796cf3671edc17265f008a55b899bb
kangchangyi <1004845773@qq.com>
2020-06-04 15:37:57 +0800

    fix(*): 修改打包的模块类型

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,20 @@
 import { terser } from "rollup-plugin-terser";
 import babel from "rollup-plugin-babel";
-import typescript from 'rollup-plugin-typescript2';
+import typescript from "rollup-plugin-typescript2";
 
 export default {
   input: "src/index.ts",
   output: {
     file: "./dist/index.js",
-    format: "cjs",
+    format: "umd",
     name: "ybj-zlb-sdk",
     sourceMap: true,
   },
   plugins: [
     typescript(),
-    terser(),
     babel({
       exclude: "node_modules/**",
     }),
+    terser(),
   ],
 };
